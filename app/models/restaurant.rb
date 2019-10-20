7a8876239893b364381c4774beae1e981f8d10f7
David Hardy <davehardy632@gmail.com>
2019-10-20 17:24:52 -0600

    Refactoring the find_post_codes method to an activerecord query rather than a ruby map method.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,23 +6,14 @@ class Restaurant < ApplicationRecord
 
 
   def self.post_code_data
-    binding.pry
     self.select("restaurants.post_code as post_code, count(restaurants.post_code) as total_places, sum(restaurants.number_of_chairs) as total_chairs, (CAST(sum(restaurants.number_of_chairs) as Float) / CAST((SELECT SUM(number_of_chairs) FROM restaurants) as Float) * 100) as chairs_pct, restaurants.name as place_with_max_chairs, MAX(number_of_chairs) as max_chairs").group("post_code").group("restaurants.name")
   end
 
-  # "SELECT restaurants.post_code as post_code, count(restaurants.post_code) as total_places, sum(restaurants.number_of_chairs) as total_chairs, (CAST(sum(restaurants.number_of_chairs) as Float) / CAST((SELECT SUM(number_of_chairs) FROM restaurants) as Float) * 100) as chairs_pct, (SELECT restaurants.name FROM restaurants WHERE restaurants.post_code = post_code ORDER BY restaurants.number_of_chairs desc LIMIT 1) as place_with_max_chairs, MAX(number_of_chairs) as max_chairs FROM restaurants GROUP BY restaurants.post_code;"
-
-
-
   def self.find_post_codes(prefix = nil)
     if prefix
-      ls1s = all.map {|restaurant| if restaurant.post_code.include?(prefix) then restaurant end}
-      ls1s.delete(nil)
-      ls1s
+      Restaurant.where("post_code LIKE ?", "#{prefix}%")
     else
-      other = all.map {|restaurant| if !restaurant.post_code.include?("LS1") && !restaurant.post_code.include?("LS2") then restaurant end }
-      other.delete(nil)
-      other
+      Restaurant.where.not("post_code LIKE 'LS1%' OR post_code LIKE 'LS2%'")
     end
   end
 
